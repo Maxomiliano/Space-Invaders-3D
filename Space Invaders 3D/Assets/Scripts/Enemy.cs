8f96d952f147aa0225ae31efc31517d50408c66f
Maxolote <max.nebula1@gmail.com>
2023-03-13 23:21:29 -0300

    Last progress with waves prefabs in timeline

--- Space Invaders 3D/Assets/Scripts/Enemy.cs
+++ Space Invaders 3D/Assets/Scripts/Enemy.cs
@@ -7,17 +7,19 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] GameObject hitVFX;
-    [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
     [SerializeField] int hitPoints = 100;
 
     ScoreBoard scoreBoard;
+    GameObject parentGameObject;
+
 
     private void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
         AddRigidbody();
         //gameObject.AddComponent<Rigidbody>().useGravity= false;
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
     }
 
     private void AddRigidbody()
@@ -37,7 +39,7 @@ public class Enemy : MonoBehaviour
     private void ProcessHit()
     {
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         hitPoints -= 10;
         scoreBoard.IncreaseScore(scorePerHit);
     }
@@ -45,7 +47,7 @@ public class Enemy : MonoBehaviour
     private void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
     }
 
